2c9b3a3833d1282bf33bad00daf3c97b1b896eb3
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-05-01 16:33:08 +0300

    Fix rendering for Arkstone Glass and Blossom Vines
    Also Barbed Bush and Barbed Bush Plant

--- src/main/java/com/remodstudios/iron_fist/blocks/IronFistBlocks.java
+++ src/main/java/com/remodstudios/iron_fist/blocks/IronFistBlocks.java
@@ -2,9 +2,11 @@ package com.remodstudios.iron_fist.blocks;
 
 import com.remodstudios.iron_fist.IronFistMain;
 import com.remodstudios.yarnandneedles.blocks.BlockRegistry;
+import com.remodstudios.yarnandneedles.datagen.ResourceGenerators;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.*;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.sound.BlockSoundGroup;
 
 @SuppressWarnings("unused")
@@ -18,16 +20,21 @@ public class IronFistBlocks extends BlockRegistry {
     //region Block Declaration
     // Stuff
 
+    // FIXME referencing RenderLayer (client-only) in shared code!!!! -ADCLeo
     public final Block ARKSTONE_GLASS = add("arkstone_glass",
-            new Block(newCinderiteLike())
+            RegistrySettings.of(ResourceGenerators.SIMPLE_BLOCK, RenderLayer.getTranslucent()),
+            new GlassBlock(newCinderiteLike().nonOpaque())
     );
     public final Block ARKSTONE_ORE = add("arkstone_ore",
             new Block(newCinderiteLike())
     );
+    // TODO create ResourceGenerator for bush-like blocks
     public final Block BARBED_BUSH = add("barbed_bush",
+            RegistrySettings.of(ResourceGenerators.SIMPLE_BLOCK, RenderLayer.getCutout()),
             new FlowerLikeBushBlock(FabricBlockSettings.copyOf(Blocks.DEAD_BUSH))
     );
     public final Block BARBED_BUSH_PLANT = add("barbed_bush_plant",
+            RegistrySettings.of(ResourceGenerators.SIMPLE_BLOCK, RenderLayer.getCutout()),
             new FlowerLikeBushBlock(FabricBlockSettings.copyOf(Blocks.DEAD_BUSH))
     );
     public final Block BLOCK_OF_ERODITE = add("block_of_erodite",
@@ -40,6 +47,7 @@ public class IronFistBlocks extends BlockRegistry {
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK))
     );
     public final Block BLOSSOM_VINES = add("blossom_vines",
+            RegistrySettings.of(ResourceGenerators.SIMPLE_BLOCK, RenderLayer.getCutout()),
             new VineBlock(FabricBlockSettings.copyOf(Blocks.VINE))
     );
 
