b2f5e131517d9819d00d8d2f74fa69bb488882f3
LePichu <ishat.prsnl65844@gmail.com>
2021-05-01 19:34:08 +0530

    crying obsidian mechanics

--- src/main/java/com/remodstudios/iron_fist/mixins/CryingObsidianInteractionMixin.java
+++ src/main/java/com/remodstudios/iron_fist/mixins/CryingObsidianInteractionMixin.java
@@ -3,10 +3,12 @@ package com.remodstudios.iron_fist.mixins;
 import com.remodstudios.iron_fist.items.IronFistItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.CryingObsidianBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -24,6 +26,8 @@ public class CryingObsidianInteractionMixin extends Block {
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (player.getStackInHand(hand).isOf(Items.BUCKET)) {
             player.setStackInHand(hand, new ItemStack(IronFistItems.INSTANCE.BUCKET_OF_TEARS));
+            player.playSound(SoundEvents.ITEM_BUCKET_FILL, 1.0F, 1.0F);
+            world.setBlockState(pos, Blocks.OBSIDIAN.getDefaultState());
             return ActionResult.success(world.isClient());
         }
         return ActionResult.PASS;
